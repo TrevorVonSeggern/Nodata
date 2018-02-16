0156580ca981e3e5a77d2a2befdd4f5f5a696c4e
Trevor Von Seggern <tvoman2@gmail.com>
2018-02-15 21:32:58 -0700

    init

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace nodata.Controllers
+{
+    using Models;
+    
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IQueryable<MyDto> Get([FromQuery] Filter<MyDto> f)
+        {
+            var result = new List<MyDto>{
+                new MyDto{id = 1, Name = "one"},
+                new MyDto{id = 2, Name = "two"}
+            };
+
+            return f.ApplyTo(result.AsQueryable());
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
